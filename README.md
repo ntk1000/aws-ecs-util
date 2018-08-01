3a4e94d112230a20b808e380cef1c440fc400540
jun.asano <jun22011982@gmail.com>
2018-08-01 09:25:19 +0900

    add README, args, flags

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # aws-ecs-util
-utility tool for aws ecs
+
+go utility tool for aws ecs (gofe)
+
+## description
+
+* show desired,pending,running ecs tasks in all/specific cluster/services
+* detect desired > running ecs tasks in all/specific cluster/services
+* show service events in all/specific cluster
+* post to slack
+
+## install
+
+TODO
+
+## usage
+
+```
+# show desired,pending,running ecs tasks in all clusters
+gofe show-tasks -a
+
+# detect desired > running ecs tasks in all clusters (detect errors)
+gofe show-tasks -a -e
+
+# filter cluster-name with sc option
+gofe show-tasks -c cluster-name
+
+# filter service-name with ss option
+gofe show-tasks -s service-name
+
+# show service events
+gofe show-events
+
+```
+
+### ref
+
+* https://deeeet.com/writing/2014/08/27/cli-reference/
+* http://yapcasia.org/2014/talk/show/b49cc53a-027b-11e4-9357-07b16aeab6a4
+* https://www.gnu.org/prep/standards/html_node/Option-Table.html#Option-Table
+
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"fmt"
 	"os"
 
@@ -10,17 +12,80 @@ import (
 	"github.com/aws/aws-sdk-go/service/ecs"
 )
 
+const (
+	Version       = "v 0.0.1"
+	TaskCommand   = "show-tasks"
+	EventsCommand = "show-events"
+)
+
+// command and options
+var (
+	Command     string
+	AllFlag     = flag.Bool("a", false, "use this option for all clusters")
+	ErrorFlag   = flag.Bool("e", false, "detect desired > running ecs tasks")
+	ClusterFlag = flag.String("c", "", "filter by cluster name")
+	ServiceFlag = flag.String("s", "", "filter by service name")
+	VersionFlag = flag.String("v", "", Version)
+)
+
+func exitErrorf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format+"\n", args...)
+	os.Exit(1)
+}
+
 func main() {
 
+	Init()
+
+	Run()
+
+}
+
+// Init checks and parses args
+func Init() {
+	// check args
+	if len(os.Args) < 2 {
+		exitErrorf("Usage: gofe <command> <options>")
+	}
+
+	Command = os.Args[1]
+	err := CheckCommand(Command)
+	if err != nil {
+		exitErrorf("%v", err)
+	}
+
+	flag.Parse()
+}
+
+// CheckCommand checks command string
+func CheckCommand(command string) error {
+	switch command {
+	case TaskCommand:
+		return nil
+	case EventsCommand:
+		return nil
+	case "-h":
+		return nil
+	case "-v":
+		return nil
+	default:
+		return errors.New("unsupported command")
+	}
+
+}
+
+// Run example
+// TODO error handling
+func Run() {
 	// create client
 	svc := CreateServiceClientViaEnv()
 
 	// list clusters
-	lsc, _ := LsClusters(svc)
+	lsc, _ := ListClusters(svc)
 
 	// describe services
 	for _, v := range lsc.ClusterArns {
-		lss, _ := LsServices(svc, v)
+		lss, _ := ListServices(svc, v)
 		dss, _ := DescServices(svc, v, lss.ServiceArns)
 		for _, vv := range dss.Services {
 			// stdout if desired <> running
@@ -34,6 +99,7 @@ func main() {
 			}
 		}
 	}
+
 }
 
 // CreateServiceClient returns ECS client via env
@@ -47,18 +113,21 @@ func CreateServiceClientViaEnv() *ecs.ECS {
 	})
 }
 
-func LsClusters(svc *ecs.ECS) (*ecs.ListClustersOutput, error) {
+// ListClusters returns ECS clustersoutput
+func ListClusters(svc *ecs.ECS) (*ecs.ListClustersOutput, error) {
 	input := &ecs.ListClustersInput{}
 	return svc.ListClusters(input)
 }
 
-func LsServices(svc *ecs.ECS, cluster *string) (*ecs.ListServicesOutput, error) {
+// ListServices returns ECS serviceoutput
+func ListServices(svc *ecs.ECS, cluster *string) (*ecs.ListServicesOutput, error) {
 	input := &ecs.ListServicesInput{
 		Cluster: cluster,
 	}
 	return svc.ListServices(input)
 }
 
+// DescServices returns ECS services
 func DescServices(svc *ecs.ECS, cluster *string, service []*string) (*ecs.DescribeServicesOutput, error) {
 	input := &ecs.DescribeServicesInput{
 		Cluster:  cluster,
@@ -67,6 +136,7 @@ func DescServices(svc *ecs.ECS, cluster *string, service []*string) (*ecs.Descri
 	return svc.DescribeServices(input)
 }
 
+// ServiceStatus is minimum struct of ECS ServiceStatus
 type ServiceStatus struct {
 	ClusterArn     *string
 	ServiceName    *string
