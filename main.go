e85fcfa857f8257424cd99e53e568b187e2bc3c3
jun.asano <jun22011982@gmail.com>
2018-08-01 14:10:41 +0900

    remove args, use flag

--- main.go
+++ main.go
@@ -13,7 +13,6 @@ import (
 )
 
 const (
-	Version       = "v 0.0.1"
 	TaskCommand   = "show-tasks"
 	EventsCommand = "show-events"
 )
@@ -21,40 +20,37 @@ const (
 // command and options
 var (
 	Command     string
+	CommandFlag = flag.String("c", "", "command")
 	AllFlag     = flag.Bool("a", false, "use this option for all clusters")
 	ErrorFlag   = flag.Bool("e", false, "detect desired > running ecs tasks")
-	ClusterFlag = flag.String("c", "", "filter by cluster name")
-	ServiceFlag = flag.String("s", "", "filter by service name")
-	VersionFlag = flag.String("v", "", Version)
+	ClusterFlag = flag.String("cn", "", "filter by cluster name")
+	ServiceFlag = flag.String("sn", "", "filter by service name")
 )
 
+func main() {
+	Init()
+	Run()
+}
+
+// error helper function
 func exitErrorf(format string, args ...interface{}) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
 	os.Exit(1)
-}
-
-func main() {
-
-	Init()
-
-	Run()
-
 }
 
 // Init checks and parses args
 func Init() {
 	// check args
 	if len(os.Args) < 2 {
-		exitErrorf("Usage: gofe <command> <options>")
+		exitErrorf("Usage: gofe -p <command> <options>")
 	}
-
-	Command = os.Args[1]
+	flag.Parse()
+	PrintFlags()
+	Command = *CommandFlag
 	err := CheckCommand(Command)
 	if err != nil {
 		exitErrorf("%v", err)
 	}
-
-	flag.Parse()
 }
 
 // CheckCommand checks command string
@@ -64,29 +60,68 @@ func CheckCommand(command string) error {
 		return nil
 	case EventsCommand:
 		return nil
-	case "-h":
-		return nil
-	case "-v":
-		return nil
 	default:
 		return errors.New("unsupported command")
 	}
+}
 
+func PrintFlags() {
+	fmt.Println(*CommandFlag)
+	fmt.Println(*AllFlag)
+	fmt.Println(*ErrorFlag)
+	fmt.Println(*ClusterFlag)
+	fmt.Println(*ServiceFlag)
+}
+
+func StdOutService(service ecs.Service) {
+	fmt.Printf("cluster = %+v\n", *service.ClusterArn)
+	fmt.Printf("service = %+v\n", *service.ServiceName)
+	fmt.Printf("taskdefinition = %+v\n", *service.TaskDefinition)
+	fmt.Printf("desired = %+v\n", *service.DesiredCount)
+	fmt.Printf("pending = %+v\n", *service.PendingCount)
+	fmt.Printf("running = %+v\n", *service.RunningCount)
 }
 
 // Run example
-// TODO error handling
 func Run() {
 	// create client
 	svc := CreateServiceClientViaEnv()
 
+	var clusters *ecs.ListClustersOutput
+	// run command
+	if Command == TaskCommand {
+		if *AllFlag {
+			fmt.Printf("all")
+			clusters = ListClusters(svc)
+
+			for _, cluster := range clusters.ClusterArns {
+				services := ListServices(svc, cluster)
+				descs := DescServices(svc, cluster, services.ServiceArns)
+				if *ErrorFlag {
+					for _, service := range descs.Services {
+						if *service.DesiredCount != *service.RunningCount {
+							StdOutService(*service)
+						}
+					}
+				} else {
+					for _, service := range descs.Services {
+						StdOutService(*service)
+					}
+				}
+			}
+		}
+
+	} else if Command == EventsCommand {
+		fmt.Printf(Command)
+	}
+
 	// list clusters
-	lsc, _ := ListClusters(svc)
+	lsc := ListClusters(svc)
 
 	// describe services
 	for _, v := range lsc.ClusterArns {
-		lss, _ := ListServices(svc, v)
-		dss, _ := DescServices(svc, v, lss.ServiceArns)
+		lss := ListServices(svc, v)
+		dss := DescServices(svc, v, lss.ServiceArns)
 		for _, vv := range dss.Services {
 			// stdout if desired <> running
 			if *vv.DesiredCount != *vv.RunningCount {
@@ -106,7 +141,11 @@ func Run() {
 func CreateServiceClientViaEnv() *ecs.ECS {
 	sess := session.Must(session.NewSession())
 	creds := credentials.NewEnvCredentials()
-	creds.Get()
+	_, err := creds.Get()
+	// exit if AWS_ACCESS_KEY_ID or AWS_SECRET_ACCESS_KEY env not defined
+	if err != nil {
+		exitErrorf("%v", err)
+	}
 	return ecs.New(sess, &aws.Config{
 		Credentials: creds,
 		Region:      aws.String(os.Getenv("AWS_DEFAULT_REGION")),
@@ -114,34 +153,45 @@ func CreateServiceClientViaEnv() *ecs.ECS {
 }
 
 // ListClusters returns ECS clustersoutput
-func ListClusters(svc *ecs.ECS) (*ecs.ListClustersOutput, error) {
+func ListClusters(svc *ecs.ECS) (list *ecs.ListClustersOutput) {
 	input := &ecs.ListClustersInput{}
-	return svc.ListClusters(input)
+	list, err := svc.ListClusters(input)
+	if err != nil {
+		exitErrorf("%v", err)
+	}
+	return
 }
 
 // ListServices returns ECS serviceoutput
-func ListServices(svc *ecs.ECS, cluster *string) (*ecs.ListServicesOutput, error) {
+func ListServices(svc *ecs.ECS, cluster *string) (list *ecs.ListServicesOutput) {
 	input := &ecs.ListServicesInput{
 		Cluster: cluster,
 	}
-	return svc.ListServices(input)
+	list, err := svc.ListServices(input)
+	if err != nil {
+		exitErrorf("%v", err)
+	}
+	return
 }
 
 // DescServices returns ECS services
-func DescServices(svc *ecs.ECS, cluster *string, service []*string) (*ecs.DescribeServicesOutput, error) {
+func DescServices(svc *ecs.ECS, cluster *string, service []*string) (desc *ecs.DescribeServicesOutput) {
+
+	// debug
+	//fmt.Printf("svc %v\n", svc)
+	//fmt.Printf("cluster %v\n", *cluster)
+	//for _, v := range service {
+	//	fmt.Printf("service %v\n", *v)
+	//}
+
 	input := &ecs.DescribeServicesInput{
 		Cluster:  cluster,
 		Services: service,
 	}
-	return svc.DescribeServices(input)
-}
-
-// ServiceStatus is minimum struct of ECS ServiceStatus
-type ServiceStatus struct {
-	ClusterArn     *string
-	ServiceName    *string
-	TaskDefinition *string
-	DesiredCount   *int64
-	PendingCount   *int64
-	RunningCount   *int64
+	desc, err := svc.DescribeServices(input)
+	if err != nil {
+		// dont exit if cluster has no services
+		fmt.Errorf("%v", err)
+	}
+	return
 }
